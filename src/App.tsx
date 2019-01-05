e7d25d7438f3519d4684987c2fdca50ea9816b04
a.sosnovskiy <a.sosnovskiy@2gis.ru>
2019-01-05 22:39:46 +0700

    redux infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {MuiThemeProvider} from '@material-ui/core/styles';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import * as React from 'react';
 import createTheme from './Theme';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import App from './App';
+import rootReducer from './Reducers'
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-  <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
